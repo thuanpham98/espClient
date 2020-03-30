f173dd25d30732b22357278e90a78f0519c37dbb
Thuan <pmttmp24@gmail.com>
2020-03-30 19:17:05 +0700

    full 10 sensor

--- main/main.c
+++ main/main.c
@@ -40,6 +40,7 @@
 
 /* library for perihape */
 #include "driver/i2c.h"
+#include <driver/adc.h>
 
 /* define from file Konfig */
 #define ESP_WIFI_SSID               CONFIG_WIFI_SSID
@@ -51,102 +52,25 @@
 #define ESP_NUM                     CONFIG_NUMBER_DEVICE
 
 /* define for I2C */
-static const char *TAG_I2C = "i2c";
+#define I2C_MASTER_SCL_IO           22            
+#define I2C_MASTER_SDA_IO           21               
+#define I2C_MASTER_FREQ_HZ          400000       
+#define I2C_MASTER_TX_BUF_DISABLE   0                          
+#define I2C_MASTER_RX_BUF_DISABLE   0                        
+#define WRITE_BIT                   I2C_MASTER_WRITE             
+#define READ_BIT                    I2C_MASTER_READ                
+#define ACK_CHECK_EN                0x1                       
+#define ACK_CHECK_DIS               0x0                       
+#define ACK_VAL                     0x0                             
+#define NACK_VAL                    0x1                        
+#define I2C_MASTER_NUM              I2C_NUM_0
 
-#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
-#define I2C_MASTER_FREQ_HZ 400000        /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define ESP_SLAVE_ADDR              0x40   /*!< slave address for DHT21 sensor */
+#define HTU21D_CRC8_POLYNOMINAL     0x13100   /*!>crc8 polynomial for 16bit value, CRC8 -> x^8 + x^5 + x^4 + 1 */
 
-#define ESP_SLAVE_ADDR 0x40   /*!< slave address for DHT21 sensor */
-
-#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
-#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
-#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
-#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
-#define ACK_VAL 0x0                             /*!< I2C ack value */
-#define NACK_VAL 0x1                            /*!< I2C nack value */
-
-#define I2C_MASTER_NUM I2C_NUM_0
-#define HTU21D_CRC8_POLYNOMINAL      0x13100   //crc8 polynomial for 16bit value, CRC8 -> x^8 + x^5 + x^4 + 1
-
+/* buffer read/write for i2c */
 uint8_t data_write[2];
 uint8_t data_read[3];
-
-/**
- * @brief test code to read esp-i2c-slave
- *        We need to fill the buffer of esp slave device, then master can read them out.
- *
- * _______________________________________________________________________________________
- * | start | slave_addr + rd_bit +ack | read n-1 bytes + ack | read 1 byte + nack | stop |
- * --------|--------------------------|----------------------|--------------------|------|
- *
- */
-static esp_err_t i2c_master_read_slave(i2c_port_t i2c_num, uint8_t *data_rd, size_t size)
-{
-    if (size == 0) {
-        return ESP_OK;
-    }
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | READ_BIT, ACK_CHECK_EN);
-    if (size > 1) {
-        i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
-    }
-    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-    i2c_cmd_link_delete(cmd);
-    return ret;
-}
-/**
- * @brief Test code to write esp-i2c-slave
- *        Master device write data to slave(both esp32),
- *        the data will be stored in slave buffer.
- *        We can read them out from slave buffer.
- *
- * ___________________________________________________________________
- * | start | slave_addr + wr_bit + ack | write n bytes + ack  | stop |
- * --------|---------------------------|----------------------|------|
- *
- */
-static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
-{
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | WRITE_BIT, ACK_CHECK_EN);
-    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
-    i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
-    i2c_cmd_link_delete(cmd);
-    return ret;
-}
-/**
- * @brief i2c master initialization
- */
-static esp_err_t i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_io_num = I2C_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-    i2c_param_config(i2c_master_port, &conf);
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
-uint8_t checkCRC8(uint16_t data)
-{
-    for (uint8_t bit = 0; bit < 16; bit++)
-    {
-        if   (data & 0x8000) data = (data << 1) ^ HTU21D_CRC8_POLYNOMINAL;
-        else data <<= 1;
-    }
-    return data >>= 8;
-}
 
 /* define bit in eventgroup, which determine event wifi connected/disconnected */
 #define WIFI_CONNECTED_BIT BIT0
@@ -158,6 +82,7 @@ static EventGroupHandle_t s_wifi_event_group;
 /* TAG to log into screen */
 static const char *TAG_HTTP = "HTTP";
 static const char *TAG_WIFI = "WIFI";
+static const char *TAG_I2C = "i2c";
 
 /* variable temp to count times reconnect wifi */
 static int s_retry_num = 0;
@@ -166,6 +91,10 @@ static int s_retry_num = 0;
 char temp[ESP_MAX_HTTP_RECV_BUFFER];
 
 /* Alloc function here to easy see */
+uint8_t checkCRC8(uint16_t data);
+static esp_err_t i2c_master_init(void);
+static esp_err_t i2c_master_read_slave(i2c_port_t i2c_num, uint8_t *data_rd, size_t size);
+static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size);
 char* Print_JSON(char* id,double data[10]);
 void wifi_init_sta(void);
 void postTask(void *pv);
@@ -291,7 +220,7 @@ void wifi_init_sta(void)
     vEventGroupDelete(s_wifi_event_group);
 }
 
-/////------------make json to post------------------///
+/* make json to post */
 char* Print_JSON(char* id,double data[10])
 {
     cJSON* sudo =cJSON_CreateObject();
@@ -315,7 +244,7 @@ char* Print_JSON(char* id,double data[10])
     cJSON_Delete(sudo);//if don't free, heap memory will be overload
     return a;
 }
-//-------Post method----------//
+/* Post method */
 void postTask (void *pv)
 {
     ESP_LOGI(TAG_HTTP," Init http Post");
@@ -378,6 +307,17 @@ void postTask (void *pv)
         i2c_master_write_slave(I2C_MASTER_NUM, &data_write[0], 1);
         i2c_master_read_slave(I2C_MASTER_NUM, data_read, 1);
 
+        //-------------------------------------------------------//
+        /* read data from analog */
+        data[2] = (double)adc1_get_raw(ADC1_CHANNEL_0)/4096*100;
+        data[3] = (double)adc1_get_raw(ADC1_CHANNEL_1)/4096*100;
+        data[4] = (double)adc1_get_raw(ADC1_CHANNEL_2)/4096*100;
+        data[5] = (double)adc1_get_raw(ADC1_CHANNEL_3)/4096*100;
+        data[6] = (double)adc1_get_raw(ADC1_CHANNEL_4)/4096*100;
+        data[7] = (double)adc1_get_raw(ADC1_CHANNEL_5)/4096*100;
+        data[8] = (double)adc1_get_raw(ADC1_CHANNEL_6)/4096*100;
+        data[9] = (double)adc1_get_raw(ADC1_CHANNEL_7)/4096*100;
+
         /* USER code end here */
 
         post_data = Print_JSON(ESP_ID,data);
@@ -390,7 +330,7 @@ void postTask (void *pv)
                     esp_http_client_get_content_length(client));
 
 
-            data[2]++;data[3]++;data[4]++;data[5]++;data[6]++;data[7]++;data[8]++;data[9]++;
+            data[6]++;data[7]++;data[8]++;data[9]++;
             free(post_data);
 
             ESP_LOGI(TAG_HTTP,"free heap size is :%d",esp_get_free_heap_size() );
@@ -447,7 +387,76 @@ void app_main(void)
     data_write[0]=0xFE; /*!> reset*/
     i2c_master_write_slave(I2C_MASTER_NUM, &data_write[0], 1);
     vTaskDelay(100/portTICK_PERIOD_MS);
-  
+
+    /* config parameter for ADC */
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_1,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_2,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_3,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_4,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_5,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(ADC1_CHANNEL_7,ADC_ATTEN_DB_11);
+
     /* start Freertos */ 
     xTaskCreate(&postTask,"postTask",4096*4,NULL,3,NULL);
+}
+
+/* checksum CRC from sensor HTU21 */
+uint8_t checkCRC8(uint16_t data)
+{
+    for (uint8_t bit = 0; bit < 16; bit++)
+    {
+        if   (data & 0x8000) data = (data << 1) ^ HTU21D_CRC8_POLYNOMINAL;
+        else data <<= 1;
+    }
+    return data >>= 8;
+}
+
+/* I2C master Initial */
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+/* I2C master read data from slave */
+static esp_err_t i2c_master_read_slave(i2c_port_t i2c_num, uint8_t *data_rd, size_t size)
+{
+    if (size == 0) {
+        return ESP_OK;
+    }
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1) {
+        i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+/* I2C master write data to slave */
+static esp_err_t i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (ESP_SLAVE_ADDR << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
 }
