daa5a2fa917e640170732b5d4568a43f1336d4fb
Thuan <pmttmp24@gmail.com>
2020-04-08 11:24:51 +0700

    update more device

--- main/main.c
+++ main/main.c
@@ -219,6 +219,15 @@ void getTask(void *pv)
     esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_http_client_set_method(client, HTTP_METHOD_GET);
 
+    char *a = (char* ) malloc(15*sizeof(char));
+    char *b = (char* ) malloc(10*sizeof(char));
+    
+    strcpy(a,ESP_ID);
+    itoa(ESP_NUM,b,10);
+    strcat(a,b);
+    
+    esp_http_client_set_header(client, "ID",a );
+
     esp_http_client_set_header(client, "ID", ESP_ID);
     ESP_LOGI(TAG_HTTP, " started");
     while (1)
