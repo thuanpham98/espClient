bdff1dc8cdd5ab542210bce5c986c84cc6650cca
thuan <pmttmp24@gmail.com>
2020-06-21 14:22:13 +0700

    update

--- main/main.c
+++ main/main.c
@@ -9,73 +9,103 @@
 #include "esp_log.h"
 #include "driver/can.h"
 
-static const char *TAG="semaphot";
+static const char *TAG="QUEUE_TEST";
 
-// void master(void *pv)
-// {
-//     //Configure message to transmit
-//     can_message_t message;
-//     message.identifier = 0x123;
-//     message.data_length_code = 8;
-//     message.flags=CAN_MSG_FLAG_NONE;
-//     for (int i = 0; i < 8; i++) {
-//         message.data[i] = i;
-//     }
-//     while (1)
-//     {
-//         //Queue message for transmission
-//         if (can_transmit(&message, 100/portTICK_PERIOD_MS) == ESP_OK) {
-//             ESP_LOGI(TAG,"Message queued for transmission");
-//             can_clear_transmit_queue();
-//         } else {
-//             ESP_LOGE(TAG,"Failed to queue message for transmission");
-//         }
-//         vTaskDelay(1000/portTICK_PERIOD_MS);
-//     }
-//     vTaskDelete(NULL);
-// }
+QueueHandle_t my_queue;
+SemaphoreHandle_t my_sema;
 
-void slave(void *pv)
+typedef struct
 {
-    can_message_t message;
+    char header[15];
+    uint32_t ID;
+    float val1;
+} frame ;
+
+
+void task1(void *pv)
+{
+    ESP_LOGI(TAG,"start task1");
+    frame data_send;
+    uint8_t buffer[sizeof(frame)+1];
+    memcpy(data_send.header,"thuandeptrai69",sizeof(data_send.header));
+    data_send.ID=1232154;
+    
+    data_send.val1=12.21;
+    memcpy(buffer,&data_send,sizeof(frame));
+    xQueueSendToBack(my_queue,(void *)buffer,100/portTICK_PERIOD_MS);
+    
+    data_send.val1 ++;
+    memcpy(buffer,&data_send,sizeof(frame));
+    xQueueSendToBack(my_queue,(void *)buffer,100/portTICK_PERIOD_MS);
+    
+    // data_send.val1 ++;
+    // memcpy(buffer,&data_send,sizeof(frame));
+    // xQueueSendToBack(my_queue,(void *)buffer,100/portTICK_PERIOD_MS);
+
     while (1)
     {
-        can_clear_receive_queue();
-        if(can_receive(&message,portMAX_DELAY)==ESP_OK)
+        if(xSemaphoreTakeRecursive(my_sema,100/portTICK_PERIOD_MS))
         {
-            ESP_LOGI(TAG,"%s",message.data);
+            ESP_LOGI(TAG,"send done nha");
+            xSemaphoreGiveRecursive(my_sema);
         }
-        else
-        {
-            ESP_LOGE(TAG,"waiting message");
-        }
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        
     }
+    
+}
+void todo(void *pv)
+{
+    uint8_t buffer[sizeof(frame)+1];
+    frame data_receive;
+    if(xQueueReceive(my_queue,(void *)buffer,portMAX_DELAY))
+    {
+        memcpy(&data_receive,buffer,sizeof(frame));
+        ESP_LOGI(TAG,"to do value : %f",data_receive.val1);
+    }
+    else
+    {
+        ESP_LOGE(TAG,"do not receive anything");
+    }
+    ESP_LOGI(TAG,"Done todo");
     vTaskDelete(NULL);
 }
+void task2(void *pv)
+{
+    ESP_LOGI(TAG,"start task2");
+    uint8_t buffer[sizeof(frame)+1];
+    frame data_receive;
+    while (1)
+    {
+        if(my_queue !=0)
+        {
+            if(xQueueReceive(my_queue,(void *)buffer,portMAX_DELAY))
+            {
+                xSemaphoreGiveRecursive(my_sema);
+                memcpy(&data_receive,buffer,sizeof(frame));
+                ESP_LOGI(TAG,"value : %f",data_receive.val1);
+                if(xSemaphoreTakeRecursive(my_sema,portMAX_DELAY))
+                {
+                    xTaskCreate(todo,"todo",4096,NULL,4,NULL);
+                    xSemaphoreGiveRecursive(my_sema);
+                }   
+            }
+        }
+
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
+
 
 void app_main(void)
 {
-    //Initialize configuration structures using macro initializers
-    can_general_config_t g_config = CAN_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, CAN_MODE_NORMAL);
-    can_timing_config_t t_config = CAN_TIMING_CONFIG_500KBITS();
-    can_filter_config_t f_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
+    my_sema=xSemaphoreCreateMutex();
+    my_queue=xQueueCreate(3,sizeof(frame)+1);
 
-    //Install CAN driver
-    if (can_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
-        ESP_LOGI(TAG,"Driver installed");
-    } else {
-        ESP_LOGE(TAG,"Failed to install driver");
-        return;
-    }
-
-    //Start CAN driver
-    if (can_start() == ESP_OK) {
-        ESP_LOGI(TAG,"Driver started");
-    } else {
-        ESP_LOGE(TAG,"Failed to start driver");
-        return;
-    }
-
-    // xTaskCreate(master,"master",4096*2,NULL,3,NULL);
-    xTaskCreate(slave,"slave",4096*2,NULL,3,NULL);
+    xTaskCreate(task1,"task1",4096*2,NULL,3,NULL);
+    xTaskCreate(task2,"task2",4096*2,NULL,3,NULL);
+    
+    
+    // xTaskCreate(task3,"task3",4096*2,NULL,3,NULL);
+     
 }
