72203650e039a5d75db30baa17661fc5d89e5725
thuan <pmttmp24@gmail.com>
2020-06-22 14:15:49 +0700

    update

--- main/main.c
+++ main/main.c
@@ -8,90 +8,68 @@
 #include "esp_err.h"
 #include "esp_log.h"
 
-#include "driver/can.h"
 #include "driver/gpio.h"
+#include "driver/spi_common.h"
+#include "driver/spi_common_internal.h"
+#include "driver/spi_master.h"
 
-#include "esp_intr_alloc.h"
-#include "soc/soc.h"
-static const char *TAG="INTR_TEST";
-
-QueueHandle_t my_queue;
-SemaphoreHandle_t my_sema;
-
-intr_handle_t my_intr;
-intr_handle_t my_intr_ex;
-
-typedef struct
-{
-    char header[15];
-    uint32_t ID;
-    float val1;
-} frame ;
-int count =0;
-void gpio_ex_idrq(void *para)
-{
-    count ++;
-}
-
-void task1(void *pv)
-{
-    ESP_LOGI(TAG,"start task1");
-    while (1)
-    {
-
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        
-    }
-    
-}
-
-void task2(void *pv)
-{
-    ESP_ERROR_CHECK(esp_intr_enable(my_intr_ex));
-
-    int a;
-    // a= esp_intr_get_cpu(my_intr);
-    // ESP_LOGI(TAG,"%d",a);
-    a=esp_intr_get_cpu(my_intr_ex);
-    ESP_LOGI(TAG,"%d",a);
-
-    gpio_config_t cf ={
-        .pin_bit_mask=1 <<GPIO_NUM_21,     
-        .mode=GPIO_MODE_INPUT,               
-        .pull_up_en=0,   
-        .pull_down_en=0,                                    
-        .intr_type=GPIO_INTR_HIGH_LEVEL     
-    };
-    gpio_config(&cf);
-    while (1)
-    {
-        ESP_LOGI(TAG,"task2 do to");
-
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
-}
+static const char *TAG = "SPI_TEST";
 
 void todo(void *pv)
-{
-        while (1)
+{    
+    spi_device_interface_config_t dev_cf;
+    dev_cf.clock_speed_hz = SPI_MASTER_FREQ_10M;
+    dev_cf.mode = 0;
+    dev_cf.spics_io_num = 5;
+    dev_cf.queue_size = 1;
+    dev_cf.flags=SPI_DEVICE_3WIRE;
+
+    spi_device_handle_t spi_hand;
+
+    spi_bus_add_device(SPI2_HOST, &dev_cf, &spi_hand);
+
+    spi_transaction_t data;
+    memset(&data,0,sizeof(data));
+    uint8_t data_transmit[4]={1,2,3,4}, data_receive[4];
+
+    // data.flags=SPI_TRANS_MODE_DIO;
+    data.length=32;
+    data.tx_buffer=data_transmit;
+    data.user=(void *)1;
+
+
+// SPI_TRANS_USE_TXDATA
+    ESP_LOGI(TAG, "Initializing task todo");
+    while (1)
     {
+        
+        ESP_LOGI(TAG, "task to do is doing");
+        spi_device_acquire_bus(spi_hand,portMAX_DELAY);
 
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        spi_device_polling_transmit(spi_hand,&data);
+
+        spi_device_release_bus(spi_hand);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-
 }
 
 void app_main(void)
 {
-    
-    // ESP_ERROR_CHECK(esp_intr_alloc(ETS_INTERNAL_TIMER1_INTR_SOURCE,ESP_INTR_FLAG_LEVEL2,NULL,NULL,&my_intr));
-    ESP_ERROR_CHECK(esp_intr_alloc(ETS_GPIO_INTR_SOURCE,ESP_INTR_FLAG_LEVEL3,NULL,NULL,&my_intr_ex));
-    // ESP_ERROR_CHECK(esp_intr_enable(my_intr));
 
+    esp_err_t ret;
 
+    ESP_LOGI(TAG, "Initializing bus SPI...");
+    spi_bus_config_t buscfg = {
+        .miso_io_num = 12,
+        .mosi_io_num = 13,
+        .sclk_io_num = 14,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 32,
+    };
+    //Initialize the SPI bus
+    ret = spi_bus_initialize(SPI2_HOST, &buscfg, 2);
+    ESP_ERROR_CHECK(ret);
 
-    xTaskCreatePinnedToCore(task1,"task1",4096*2,NULL,3,NULL,1);
-    xTaskCreatePinnedToCore(task2,"task2",4096*2,NULL,3,NULL,0);
-    xTaskCreate(todo,"todo",4096,NULL,3,NULL);
-     
+    xTaskCreate(todo, "todo", 4096 * 3, NULL, 3, NULL);
 }
