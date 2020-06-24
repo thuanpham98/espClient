f73e013148d5bb0ede5504ca494ae0efff90a081
thuan <pmttmp24@gmail.com>
2020-06-24 11:51:01 +0700

    update

--- main/main.c
+++ main/main.c
@@ -19,7 +19,6 @@ QueueHandle_t my_queue;
 SemaphoreHandle_t my_sema;
 
 intr_handle_t my_intr;
-intr_handle_t my_intr_ex;
 
 typedef struct
 {
@@ -28,41 +27,40 @@ typedef struct
     float val1;
 } frame ;
 int count =0;
-void gpio_ex_idrq(void *para)
+
+void hanlder_gpl(void *pv)
 {
-    count ++;
+    count++;
+    while(gpio_get_level(21)==1);
 }
 
-void task1(void *pv)
-{
-    ESP_LOGI(TAG,"start task1");
-    while (1)
-    {
+// void task1(void *pv)
+// {
+//     ESP_LOGI(TAG,"start task1");
+//     while (1)
+//     {
 
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+//         vTaskDelay(1000/portTICK_PERIOD_MS);
         
-    }
+//     }
     
-}
+// }
 
 void task2(void *pv)
 {
-    ESP_ERROR_CHECK(esp_intr_enable(my_intr_ex));
-
-    int a;
-    // a= esp_intr_get_cpu(my_intr);
-    // ESP_LOGI(TAG,"%d",a);
-    a=esp_intr_get_cpu(my_intr_ex);
-    ESP_LOGI(TAG,"%d",a);
 
     gpio_config_t cf ={
         .pin_bit_mask=1 <<GPIO_NUM_21,     
         .mode=GPIO_MODE_INPUT,               
         .pull_up_en=0,   
         .pull_down_en=0,                                    
-        .intr_type=GPIO_INTR_HIGH_LEVEL     
+        .intr_type=GPIO_PIN_INTR_POSEDGE    
     };
     gpio_config(&cf);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL2);
+    gpio_isr_handler_add(21,hanlder_gpl,NULL);
+
     while (1)
     {
         ESP_LOGI(TAG,"task2 do to");
@@ -76,6 +74,7 @@ void todo(void *pv)
         while (1)
     {
 
+        ESP_LOGI(TAG,"%d",count);
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
 
@@ -85,12 +84,12 @@ void app_main(void)
 {
     
     // ESP_ERROR_CHECK(esp_intr_alloc(ETS_INTERNAL_TIMER1_INTR_SOURCE,ESP_INTR_FLAG_LEVEL2,NULL,NULL,&my_intr));
-    ESP_ERROR_CHECK(esp_intr_alloc(ETS_GPIO_INTR_SOURCE,ESP_INTR_FLAG_LEVEL3,NULL,NULL,&my_intr_ex));
+    // ESP_ERROR_CHECK(esp_intr_alloc(ETS_GPIO_INTR_SOURCE,ESP_INTR_FLAG_LEVEL3,NULL,NULL,&my_intr));
     // ESP_ERROR_CHECK(esp_intr_enable(my_intr));
 
 
 
-    xTaskCreatePinnedToCore(task1,"task1",4096*2,NULL,3,NULL,1);
+    // xTaskCreatePinnedToCore(task1,"task1",4096*2,NULL,3,NULL,1);
     xTaskCreatePinnedToCore(task2,"task2",4096*2,NULL,3,NULL,0);
     xTaskCreate(todo,"todo",4096,NULL,3,NULL);
      
